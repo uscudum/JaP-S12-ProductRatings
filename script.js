df903ef5b614dcf7ef0e3b33e19ef43f1556f5a3
nicolas <nicolaser911@gmail.com>
2023-09-11 15:45:07 -0300

    completado todos los puntos

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 const URL = 'https://fakestoreapi.com/products'
+const conteiner = document.getElementById("products");
 
 document.addEventListener("DOMContentLoaded", function (e) {
     fetchData(URL)
 })
 
-async function fetchData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    products = data
+async function fetchData(URL) {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        products = data;
+        showProducts(data)    
+    } catch (error) {
+     console.log(error)   
+    }
 }
 
 
-function showProducts() {
 
+function showProducts(data) {
+    for (const producto of data) {
+        conteiner.innerHTML += `
+            <div class="list-group-item list-group-item-action cursor-active">
+                <h3>
+                    ${cutString(producto.title, 20)}
+                </h3>
+                <small class="text-muted">
+                    ${getCurrentDateTime()}
+                </small>
+                <span>
+                    ${stars(Math.trunc(producto.rating.rate))}
+                </span>
+            </div>
+        `
+    }
 }
 
-function stars(cantidad) {
+function stars(number) {
+    const stars =  '<i class="fa fa-star checked"></i>'.repeat(number) 
+    const starsEmpty = '<i class="fa fa-star"></i>'.repeat(5 - number )
+    return stars + starsEmpty
+} 
 
+function cutString(string, maxLength) {
+    if (string.length > maxLength) {
+        return string.substring(0, maxLength) + '...';
+    }
+    return string;
 }
 
-function cutString(string) {
 
+
+function getCurrentDateTime() {
+    const now = new Date();
+    return now.toLocaleString();
 }
